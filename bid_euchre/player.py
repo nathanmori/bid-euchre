97469b86dc4f6c7c385c97e82a82d23692cbf58f
Nathan Mori <nathanmori@gmail.com>
2018-06-26 23:22:58 -0700

    outline player

---
+++ bid_euchre/player.py
@@ -0,0 +1,35 @@
+"""Players."""
+
+from abc import abstractmethod
+
+class Player:
+
+    def __init__(self, team):
+        self.team = team
+        self.cards = []
+
+    @abstractmethod
+    def play_trick(self, played_cards):
+        pass
+
+    @abstractmethod
+    def bid(self, bids):
+        pass
+
+
+class CPUPlayer(Player):
+
+    def play_trick(self, played_cards):
+        pass
+
+    def bid(self, bids):
+        pass
+
+
+class UserPlayer(Player):
+
+    def play_trick(self, played_cards):
+        pass
+
+    def bid(self, bids):
+        pass
